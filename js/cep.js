6fae5d095645705cef0d0739140b99430d2670b1
Andre Tassis <tassisandre@gmail.com>
2024-02-22 10:54:20 -0300

    feat(cep): criada feature de buscar cep na api e preencher os campos dinamicamente com o resultado

---
+++ js/cep.js
@@ -0,0 +1,20 @@
+async function buscarCep() {
+    try {
+        const cep = document.getElementById('cep').value
+        const response = await fetch(`http://viacep.com.br/ws/${cep}/json`)
+        const data = await response.json()
+
+        const logradouro = document.getElementById('logradouro')
+        const bairro = document.getElementById('bairro')
+        const uf = document.getElementById('uf')
+
+        logradouro.innerText = data.logradouro
+        bairro.innerText = data.bairro
+        uf.innerText = data.uf
+    }
+
+    catch (error) {
+        error.message = "Não foi possível localizar o CEP informado. Verifique se digitou corretamente."
+        alert(error.message)
+    }
+}
