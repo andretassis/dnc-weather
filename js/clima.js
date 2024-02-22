a22fc785c7caa0ddb089da164b14cd5bda19d3c1
Andre Tassis <tassisandre@gmail.com>
2024-02-22 11:20:03 -0300

    feat(clima): criada funcao async para acessar api do open-meteo e encontrar a temperatura baseada na lat e long

--- js/clima.js
+++ js/clima.js
@@ -1,1 +1,15 @@
+async function weatherInfo() {
 
+    const lat = document.getElementById('lat').value
+    const long = document.getElementById('long').value
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`)
+    const data = await response.json()
+    console.log(data.hourly.temperature_2m[0])
+
+    const weatherResult = document.getElementById('weather-result')
+    weatherResult.innerText = `Previsão do tempo de acordo com a região: ${data.hourly.temperature_2m[0]}°C`
+}
+
+
+
+// FALTA TRATAMENTO DE ERROS AQUI!!!!
